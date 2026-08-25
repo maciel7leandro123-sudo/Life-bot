668a83919a5cc8e7103ae6de929fd5cc9f332a50
maciel7leandro123-sudo <maciel7leandro123@gmail.com>
2026-08-24 23:09:05 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Life-bot online!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+Thread(target=run, daemon=True).start()
 import discord
 from discord.ext import commands
 import datetime
