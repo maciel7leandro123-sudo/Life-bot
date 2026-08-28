38d33c2e35f9933478a66df5ddf172f51e8e6069
maciel7leandro123-sudo <maciel7leandro123@gmail.com>
2026-08-27 23:56:07 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,27 @@
-discord.py
-flask
+import os
+from flask import Flask
+import threading
+import discord
+from discord.ext import commands
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Life Bot ONLINE"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_flask).start()
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"ONLINE - Logado como {bot.user}")
+    await bot.tree.sync()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot.run(TOKEN)
